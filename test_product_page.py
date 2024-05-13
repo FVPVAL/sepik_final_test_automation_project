81dc1cbc79f7f5f576885e6ecc53586ddb8882ab
val <izol@rambler.ru>
2024-05-13 12:30:43 +0300

    параметризировали test_p

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
-import time
+import pytest
 
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('offer', [1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()

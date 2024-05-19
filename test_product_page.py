a059abcd155cf366972f68ffa07c43c44879ff07
val <izol@rambler.ru>
2024-05-19 10:09:03 +0300

    добавил пропущенный 0:)

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,7 @@ from .pages.main_page import MainPage
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('offer', [1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+@pytest.mark.parametrize('offer', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, offer):
     """Гость может добавить продукт в корзину"""
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
